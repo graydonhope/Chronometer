030d0bd26bd394571ddb101e0cf55e8e2221c168
graydonhope <41659296+graydonhope@users.noreply.github.com>
2018-10-13 16:54:40 -0400

    adding NewTaskActivity

---
+++ Chronometer/app/src/main/java/com/example/graydon/chronometer/NewTaskActivity.java
@@ -0,0 +1,37 @@
+package com.example.graydon.chronometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class NewTaskActivity extends AppCompatActivity {
+
+    //Activity - where the UI gets updated!! No logic in here
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newtask);
+        spinnerDetails();
+    }
+
+    public void spinnerDetails(){
+        Spinner spinner = (Spinner) findViewById(R.id.type_of_task);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerItems, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+    }
+
+    public void addButtonClicked(View view){
+        //create new task
+    }
+
+
+
+
+}
