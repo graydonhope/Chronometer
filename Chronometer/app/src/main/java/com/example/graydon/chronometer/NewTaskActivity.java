e1a567a062afd9e7e1fe65ccceeac5dd54fb6b67
graydonhope <41659296+graydonhope@users.noreply.github.com>
2018-10-29 20:43:28 -0400

    Updating Activity to fix double click for time selection

--- Chronometer/app/src/main/java/com/example/graydon/chronometer/NewTaskActivity.java
+++ Chronometer/app/src/main/java/com/example/graydon/chronometer/NewTaskActivity.java
@@ -1,8 +1,8 @@
 package com.example.graydon.chronometer;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
+
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,19 +11,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 
 public class NewTaskActivity extends AppCompatActivity {
 
-    //Activity - where the UI gets updated!! No logic in here
     private TextView startDateDisplay;
     private TextView endDateDisplay;
     private String taskName;
@@ -32,33 +31,110 @@ public class NewTaskActivity extends AppCompatActivity {
     private int startTimeHour = -1, endTimeHour = -1, startTimeMinute = -1, endTimeMinute = -1, reminderTime = -1;
     private boolean validNameEntered, validTimeframe, validRemindertime;
     private NewTaskModel taskModel;
+    private StoredTaskManager storedTaskManager;
+    private TimePickerDialog startTimePickerDialog, endTimePickerDialog;
+    private Spinner spinner;
+    private Context context;
+    private ArrayAdapter<CharSequence> arrayAdapter;
+    private List<String> spinnerItems = new ArrayList<String>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        StoredTaskManager.removeAllTasks(getApplicationContext());
-
+        this.context = getApplicationContext();
         super.onCreate(savedInstanceState);
+        this.storedTaskManager = new StoredTaskManager();
+        storedTaskManager.removeAllTasks(this.context);
         setContentView(R.layout.activity_newtask);
+        spinner = findViewById(R.id.typeOfTask_spinner);
+        taskModel = new NewTaskModel(NewTaskActivity.this);
+        spinnerItems = taskModel.retrieveSpinnerItems();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.context, android.R.layout.simple_spinner_item, spinnerItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
         startDateDisplay = (TextView) findViewById(R.id.startDate_textView);
         endDateDisplay = (TextView) findViewById(R.id.endDate_textView);
-        spinnerDetails();
-    }
+        startDateDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                boolean twentyfourHourTime = false;
+                TimePickerDialog dialog = new TimePickerDialog(
+                        NewTaskActivity.this, android.R.style.Theme_Holo_Light_DarkActionBar, timePickerDialogListenerStart, hourOfDay, minute, twentyfourHourTime);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+                displayStartDate();
+            }
+        });
+        endDateDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                boolean twentyfourHourTime = false;
+                TimePickerDialog dialog = new TimePickerDialog(
+                        NewTaskActivity.this, android.R.style.Theme_Holo_Light_DarkActionBar, timePickerDialogListenerEnd, hourOfDay, minute, twentyfourHourTime);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+                displayEndDate();
+            }
+        });
 
-    public void onStartTimeClick(View view){
-        Calendar calendar = Calendar.getInstance();
-        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        boolean twentyfourHourTime = false;
-        TimePickerDialog dialog = new TimePickerDialog(
-                NewTaskActivity.this, android.R.style.Theme_Holo_Light_DarkActionBar, timePickerDialogListenerStart, hourOfDay, minute, twentyfourHourTime);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.show();
-        displayStartDate();
+        timePickerDialogListenerStart = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                startTimeHour = hourOfDay;
+                startTimeMinute = minute;
+                String AM_PM;
+                String minuteToDisplay;
+                if(minute < 10){
+                    minuteToDisplay = "0" + minute;
+                }
+                else{
+                    minuteToDisplay = "" + minute;
+                }
+                if(hourOfDay < 12){
+                    AM_PM = "AM";
+                }
+                else {
+                    AM_PM = "PM";
+                }
+                String time = hourOfDay + ":" + minuteToDisplay + " " + AM_PM;
+                startDateDisplay.setText(time);
+            }
+        };
+
+        timePickerDialogListenerEnd = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                endTimeHour = hourOfDay;
+                endTimeMinute = minute;
+                String AM_PM;
+                String minuteToDisplay;
+                if(minute < 10){
+                    minuteToDisplay = "0" + minute;
+                }
+                else{
+                    minuteToDisplay = "" + minute;
+                }
+                if(hourOfDay < 12){
+                    AM_PM = "AM";
+                }
+                else {
+                    AM_PM = "PM";
+                }
+                String time = hourOfDay + ":" + minuteToDisplay + " " + AM_PM;
+                endDateDisplay.setText(time);
+            }
+        };
     }
 
     public void displayStartDate(){
-        timePickerDialogListenerStart = new TimePickerDialog.OnTimeSetListener() {
+        timePickerDialogListenerStart =  new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 startTimeHour = hourOfDay;
@@ -83,21 +159,14 @@ public class NewTaskActivity extends AppCompatActivity {
         };
     }
 
-    /**
-     *
-     * @param view
-     */
-    public void onEndTimeClick(View view){
-        Calendar calendar = Calendar.getInstance();
-        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        boolean twentyfourHourTime = false;
-        TimePickerDialog dialog = new TimePickerDialog(
-                NewTaskActivity.this, android.R.style.Theme_Holo_Light_DarkActionBar, timePickerDialogListenerEnd, hourOfDay, minute, twentyfourHourTime);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.show();
-        displayEndDate();
+
+    /*
+    public void updateTimeDisplay(int startHour, int startMinute, int endHour, int endMinute){
+        startTimePickerDialog.updateTime(startHour, startMinute);
+        endTimePickerDialog.updateTime(endHour, endMinute);
     }
+
+    */
 
     public void displayEndDate(){
         timePickerDialogListenerEnd = new TimePickerDialog.OnTimeSetListener() {
@@ -126,18 +195,8 @@ public class NewTaskActivity extends AppCompatActivity {
     }
 
 
-    public void spinnerDetails(){
-        Spinner spinner = (Spinner) findViewById(R.id.typeOfTask_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerItems, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setSelection(0);
-    }
-
-
     public void isValidTimeframe(){
         if(startTimeHour != -1 && startTimeMinute != -1 && endTimeHour != -1 && endTimeMinute != -1){
-            Log.d("!!!!!!!!!", "isValidTimeframe: startTime Minutes" + startTimeMinute + " endTime Minutes " + endTimeMinute);
             if((startTimeHour < endTimeHour) || ((startTimeHour <= endTimeHour) && startTimeMinute < endTimeMinute)){
                 validTimeframe = true;
             }
@@ -174,22 +233,24 @@ public class NewTaskActivity extends AppCompatActivity {
     public void addButtonClicked(View view){
         //create new task - need name, hours, minutes, reminder
         isValidNameEntered(); isValidReminder(); isValidTimeframe();
-        Log.d("!!!!After methods ", "addButtonClicked: ValidName:" + validNameEntered + " ValidReminder: " + validRemindertime + " valid TimeFrame: " + validTimeframe);
         if(validNameEntered && validTimeframe && validRemindertime){
             Duration startTime = new Duration(this.startTimeHour, this.startTimeMinute);
             Duration endTime = new Duration(this.endTimeHour, this.endTimeMinute);
-            taskModel = new NewTaskModel(this, startTime, endTime, taskName);
-
-            if(!(taskModel.checkNameUnique()) && taskModel.checkTimeFrame()){
-                Log.d("!!!!!XXX", "addButtonClicked: Inside seoncd if");
+            taskModel.setStartTime(startTime);
+            taskModel.setEndTime(endTime);
+            taskModel.setTaskName(taskName);
+            taskModel.setStart(startTime); taskModel.setEnd(endTime);
+            Context contextToSend = getApplicationContext();
+            if(!(taskModel.checkNameUnique(contextToSend)) && taskModel.checkTimeFrame()){
                 Task newTask = new Task(taskName, startTime, endTime, reminderTime);
                 taskModel.saveTask(this, newTask);
-                Log.d("!!!!!!!!!!!", "addButtonClicked: NEW Task ADDED and Saved!!");
                 Event event = new Event();
                 event.addTask(newTask);
+                Log.d("!!!!!!!@@@@@@@", "addButtonClicked: ADDED TASK");
                 Intent intent = new Intent(this, EventInfoProgressActivity.class);
                 intent.putExtra("Event", event);
                 startActivity(intent);
+                taskModel.addTaskToSpinner(newTask);
             }
         }
         else{
