9f493bc02a7254c6e4fc52059f9a836fd51bd67b
Graydon Hope <graydonho@gmail.com>
2018-11-25 19:43:42 -0500

    Updating time frame check

---
+++ Chronometer/app/src/test/java/com/example/graydon/chronometer/NewTaskModelTest.java
@@ -0,0 +1,23 @@
+package com.example.graydon.chronometer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NewTaskModelTest {
+
+    @Test
+    public void checkName() {
+    }
+
+    @Test
+    public void checkSavedTasks() {
+    }
+
+    @Test
+    public void checkTimeFrame() {
+        Event eventPassed = new Event();
+        Duration startDurationPassed = new Duration(20, 0);
+       // Duration endDurationPassed = new Duration()
+    }
+}
